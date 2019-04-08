64a3ab66826c1fc8bf7cfe9729c39eb317614303
Jeff <jeff.elppa@gmail.com>
2019-04-08 16:00:13 +0200

    Ajout FizzBuzz de 1 à 15

---
+++ src/main/java/com/Jeff/testgit/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.Jeff.testgit;
+
+public class FizzBuzz {
+	public static String getResult() {
+	    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
+    }
+}
